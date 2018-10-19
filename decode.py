bda2b68d2d381887cf958875294686fe3b43d52c
vinigl <vinicis.gl098@gmail.com>
2018-10-19 16:07:34 -0300

    decode

--- decode.py
+++ decode.py
@@ -4,6 +4,7 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import time
 from peakutils.plot import plot as pplot
+from matplotlib import pyplot
 
 import pickle
 import peakutils
@@ -22,7 +23,6 @@ def check(x):
             return z
     return "a"
 
-
 sgn = sgn.signalMeu()
 
 num = []
@@ -31,41 +31,56 @@ fs = 44100
 sd.default.samplerate = fs
 sd.default.channels = 1
 counter = 0
-duration = 3
-while counter<5:
-    myrecording = sd.rec(int(duration * fs))
-    # print(myrecording[0])
-    sd.wait()
-    counter +=1
-    # print(counter)
-    fft = sgn.plotFFT(myrecording[:,0],fs)
+duration = 1
+print('Gravando')
+t = np.linspace(0, duration, fs)
+while counter<10:
+
+        myrecording = sd.rec(int(duration * fs))
+        # print(myrecording)
+        plt.plot(t,myrecording[:,0])
+        sd.wait()
+        counter +=1
+        # print(counter)
+        fft = sgn.plotFFT(myrecording[:,0],fs)
 
 
 
-    x = fft[0]
-    y = fft[1]
-    ynovo=[]
-    xnovo=[]
-    # print(y[0])
+        x = fft[0]
+        y = fft[1]
+        ynovo=[]
+        xnovo=[]
+        # print(y[0])
 
-    indexes = peakutils.indexes(y, thres=100, min_dist=100, thres_abs= True)
-    peaks_y = y[indexes]
-    peaks_x = x[indexes]
+        indexes = peakutils.indexes(y, thres=100, min_dist=100, thres_abs= True)
+        indexes = indexes.tolist()
+        peaks_y = y[indexes]
+        peaks_x = x[indexes]
+        peaks_x = peaks_x.tolist()
+        peaks_y = peaks_y.tolist()
+        filter_x = []
+        filter_y = []
+        for i in range(0,len(peaks_x)):
+            # print(peaks_x[i])
+            if peaks_x[i] < 1460 and peaks_x[i]>680:
+                filter_x.append(peaks_x[i])
+                filter_y.append(peaks_y[i])
 
-    # print(peaks_y)
-    for i in range(len(peaks_x)):
-        if peaks_x[i] > 1460 or peaks_x[i]<680:
-            print(peaks_x[i])
-            peaks_x = np.delete(peaks_x, i)
-    print(peaks_x)
-    res = check(peaks_x)
-    for i in range(len(num)):
-        if num[i] == "a":
-            num.pop(i)
+        print("final :" + str(filter_x))
+        res = check(filter_x)
+        num.append(res)
+        time.sleep(1)
+
+
+
+# for i in range(0,len(num)):
+#     if num[i] == "a":
+#         num.pop(i)
 
 print(num)
 pyplot.figure(figsize=(10,6))
 pplot(x, y, indexes)
-plt.show()
+pyplot.show()
+
     # sd.play(yf)
     # sd.wait()
